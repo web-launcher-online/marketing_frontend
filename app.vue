9f4ad129368ccc05f50c9680388fb07b8608852f
Thomas William McClean <spam@tommcclean.me>
2023-01-01 20:45:38 +0000

    Basic Hero Implementation

--- app.vue
+++ app.vue
@@ -1,6 +1,33 @@
 <template>
   <div>
-    <h1>Coming Soon</h1>
-    <p>Website Under Development</p>
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+html {
+  font-family: "Quicksand", "sans-serif";
+  font-size: 18px;
+  background-color: white;
+}
+
+*, *:before, *:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.u-contain {
+  position: relative;
+  max-width: 1000px;
+  z-index: 10;
+}
+
+.u-flex {
+  display: flex;
+  align-items: center;
+}
+
+.u-flex-grow {
+  flex-grow: 1;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Hero>
+      <template v-slot:content>
+        <h1>Build your own browser start page</h1>
+          <h2>Sync your pages across all browsers and devices.</h2>
+          <h2>Enhance your productivity with focus modes</h2>
+          <h2>A beautiful place to start every session </h2>
+          <button type="button">Build your own</button>
+      </template>
+
+      <template v-slot:image>
+        <img width="400" src="/devices.png">
+      </template>
+    </Hero>
+  </div>
+</template>
