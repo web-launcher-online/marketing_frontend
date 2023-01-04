1bc9b51fc6cffe068ace91d4a9e15cc2c77eac7f
Thomas William McClean <spam@tommcclean.me>
2023-01-04 22:39:08 +0000

    Privacy Policy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -26,14 +26,14 @@
 
     <InfoBlock
       title="Analytics"
-      message="Here is how we use analytics to improve our product and your experience."
+      message="Analytics anonymously show us what features are used on our website and how they perform."
     />
 
     <TextBlock>
       <p>
-        We don't use any third party tracking software like Google Analytics or
-        any other third party.
+        We don't use any third party tracking software like Google Analytics or that of any other third party.
       </p>
+      <br>
       <p>
         We capture some basic analytics when you use our website, to do this we
         use internally developed software called
@@ -46,29 +46,48 @@
         and how they are performing so that we can focus our development where
         it matters.
       </p>
+      <br>
       <p>
-        Any information that we do need to collect for the purposes of using our
-        website is not traded or sold to any third party and when in relation to
-        analytics is always anonymous (IE not linked back to you).
+        Analytics that we collect while you use our website are not traded, shared or sold to any third party and all analytics are anonymous - meaning they cannot be linked to an individual. We keep analytics data for a period of 30 days, it is then deleted.
       </p>
+      <br>
+      <p>For example, we might count the number of times a particular app block is added to pages so that we can see which ones are most used and would benefit from further development. Or we might record how long it takes for our web pages to load so we can improve their performance over time.</p>
+    </TextBlock>
+
+    <InfoBlock
+      title="Advertising"
+      message="We are staunchly against incorporating third party advertisements within our service."
+    />
+
+    <TextBlock>
+      <p>We do not use any advertising frameworks or tools on weblauncher.online, nor do we show any advertisements on any of our pages.</p>
+      <p>We don't sell space on our website to any third party, nor do we trade information that we have collected as part of delivering our service.</p>
     </TextBlock>
 
     <InfoBlock
       title="What information do we collect?"
-      message="It is necesary to collect some information to provide our service, this is what we collect and why."
+      message="It is necessary to collect some information to provide our service, this is what we collect and why."
     />
 
     <TextBlock>
-      <p>Coming Soon.</p>
+      <p>We collect information at various points when you use our service so that we can provide you with the customised pages you have created, what information we collect depends on which services you use.</p>
+      <br>
+      <p>Should you choose to create an account or sign in, we collect an email address from you so that you can use it to sign in again in future.</p>
+      <br>
+      <p>Should you choose to utilise certain app blocks like todo lists and notes, any information entered is stored on your device or on our servers so that it can be loaded again in future. We recommend against storing any sensitive personal information in the app blocks.</p>
+      <br>
+      <p>We don't store technical related identifiers like IP Addresses, however we do store your entered locations to provide the clock and weather functionalities if you select them on your pages.</p>
     </TextBlock>
 
     <InfoBlock
-      title="Our usage of Cookies"
-      message="Cookies are a fundamental part of how the web works, these are the cookies we use to deliver our service to you."
+      title="Our usage of Cookies and Storage"
+      message="Cookies are a fundamental part of how the web works, this is how we use cookies and storage on your machine."
     />
 
     <TextBlock>
-      <p>Coming Soon.</p>
+      <p>If you choose to sign in or create an account we create a cookie on your device so that we can provide the personalised experience you expect and so we can remember that you are signed in.</p>
+      <br>
+      <p>Should you choose to use our service without signing into an account, your personalised start page configuration and data is stored in your browsers local storage, this data never leaves your device unless you choose to sign in and sync your pages across your other devices in which case we store it in our databases instead.</p>
     </TextBlock>
 
     <InfoBlock
